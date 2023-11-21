e8b8ae3d5559a4e1231234e69499486653d63c42
Thesmavis <89447143+Thesmavis@users.noreply.github.com>
2023-11-21 11:11:24 +0530

    Delete myproject directory

--- myproject/candidate/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from . models import *
-
-class candidate_ser(serializers.ModelSerializer) :
-    class Meta :
-        model = Candidatedirectory
-        fields='__all__'
